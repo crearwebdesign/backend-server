46d93c11e69efce956c17b901b6435f897a4da8f
Oscar Barrios <oscar10034@gmail.com>
2022-04-09 18:32:31 -0400

    Commit from my house

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const express = require('express');
 const app = express();
 
 app.listen(3000, () =>{
-    console.log('Servidor corriendo en el puerto ' + 3000)
+    console.log('Server running at port ' + 3000)
 })
