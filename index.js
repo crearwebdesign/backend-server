55626dc1d3b53400ec64b603ce19bfc0b6d61113
Oscar Barrios <oscar10034@gmail.com>
2022-04-10 18:50:19 -0400

    Cors y Express

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
+require('dotenv').config(); // para tomar las variables que estan en el archivo .env
+
 const express = require('express');
 
+
+const cors = require('cors');
+
+const {dbConnection} = require('./database/config');
+
+// crear el servidor express
 const app = express();
 
-app.listen(3000, () =>{
-    console.log('Server running at port ' + 3000)
+// configurar CORS
+app.use(cors());
+
+// base de datos
+dbConnection();
+
+// rutas 
+
+app.get('/', (req,res) => {
+    res.json({
+        ok : true,
+        msg : 'Hola Mundo'
+    })
+});
+
+
+// el process.env.PORT viene del require('dotenv')
+app.listen(process.env.PORT, () =>{
+    console.log('Server running at port ' + process.env.PORT)
 })
