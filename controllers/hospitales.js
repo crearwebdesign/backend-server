9792d0b3027306839be5baeae47a65c76953349d
crearwebdesign <54069542+crearwebdesign@users.noreply.github.com>
2022-06-03 18:20:35 -0400

    Crear Hospitales

--- controllers/hospitales.js
+++ controllers/hospitales.js
@@ -1,5 +1,7 @@
 const {response} = require('express');
 
+const Hospital = require('../models/hospital');
+
 const getHospitales = (req, res = response) => {
     res.json({
         ok : true,
@@ -8,11 +10,31 @@ const getHospitales = (req, res = response) => {
 };
 
 
-const crearHospital = (req, res = response) => {
-    res.json({
-        ok : true,
-        msg : 'crearHospital'
-    })
+const crearHospital = async (req, res = response) => {
+    
+    const uid = req.uid;
+    const hospital = new Hospital({
+        usuario : uid,
+        ...req.body});
+
+    
+    try {
+
+        const hospitaldb = await hospital.save();
+        res.json({
+            ok : true,
+            hospital : hospitaldb
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok : false,
+            msg : " Hable con el Administrador"
+        })
+        
+    }
+
+
 };
 
 const actualizarHospital = (req, res = response) => {
