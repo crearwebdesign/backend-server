dc3b5c70506c0411f6cf8c2dd8a23e9c7458a424
Oscar Barrios <oscar10034@gmail.com>
2022-08-09 20:38:51 -0400

    Obtener y probar Token Google

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcryptjs');
 
 const Usuario = require('../models/usuario');
 const { generarJWT } = require('../helpers/jwt');
+const { googleVerify } = require('../helpers/google-verify');
 
 
 
@@ -57,10 +58,23 @@ const login = async(req, res = response)=>{
 
 const googleSignIn = async (req, res = response) => {
 
-    res.json ({
-        ok : true,
-        msg : req.body.token
-    })
+    try {
+         const { email, name, picture } = await  googleVerify( req.body.token);
+         res.json({
+            ok : true,
+            email, name, picture 
+         })
+        
+    } catch (error) {
+        console.log(error);
+        res.status(400).json ({
+            ok : false,
+            msg : 'Token de Google no es correcto'
+        })
+        
+    }
+
+
 
 };
 
