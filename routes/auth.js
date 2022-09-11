c17f91ef76cf2edd48f8eef7166d504f1368bf7f
crearwebdesign <oscar10034@gmail.com>
2022-09-11 15:56:59 -0400

    git

--- routes/auth.js
+++ routes/auth.js
@@ -5,8 +5,9 @@
 const {Router} = require('express');
 const {check} = require('express-validator');
 
-const {login, googleSignIn} = require('../controllers/auth');
+const {login, googleSignIn, renewToken} = require('../controllers/auth');
 const {validarCampos} = require('../middlewares/validar-campos');
+const { validarJWT } = require('../middlewares/validar-jwt');
 
 
 const router = Router();
@@ -22,6 +23,10 @@ router.post ('/google',[
     check('token','El Token de Google es Obligatorio').not().isEmpty(),
     validarCampos
 ], googleSignIn);
+
+router.get ('/renew',
+            validarJWT,
+            renewToken);
 
 
 
