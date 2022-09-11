8c5800fb2baa0c13cd50ea5d4a1c34e14815a8f8
unknown <oscar10034@gmail.com>
2022-09-11 11:34:31 -0400

    Seccion 12 Google

--- controllers/auth.js
+++ controllers/auth.js
@@ -60,16 +60,40 @@ const googleSignIn = async (req, res = response) => {
 
     try {
          const { email, name, picture } = await  googleVerify( req.body.token);
+         const usuarioDB = await Usuario.findOne({email});
+         let usuario;
+
+         if (!usuarioDB){
+            usuario = new Usuario({
+                nombre : name,
+                email,
+                password : '@@@',
+                img : picture,
+                google : true
+            })
+         }else{
+             usuario = usuarioDB;
+             usuario.google = true;
+         };
+
+         // Guardar Usuario
+         await usuario.save();
+
+         // Generar el Tokehn - JWT
+
+         const token = await generarJWT(usuario.id);
+
          res.json({
             ok : true,
-            email, name, picture 
+            email, name, picture,
+            token
          })
         
     } catch (error) {
         console.log(error);
         res.status(400).json ({
             ok : false,
-            msg : 'Token de Google no es correcto'
+            msg : 'Token de Google no es correcto o Falla creando Ussuario'
         })
         
     }
