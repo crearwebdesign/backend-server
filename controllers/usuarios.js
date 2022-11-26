4ce914c54648d49a550f689cd04696e405de20a4
crearwebdesign <oscar10034@gmail.com>
2022-11-26 16:58:49 -0400

    AjusteActualizarUsuario

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -94,7 +94,15 @@ const actualizarUsuario = async(req, res = response) => {
             }
         };
 
-         campos.email = email;
+        if (!usuarioDB.google){
+            campos.email = email;
+        }else if (usuarioDB.email !== email){
+            return res.status(400).json({
+                ok : false,
+                msg : 'Usuario de Google no pueden cambiar su correo'
+            })
+
+        };
 
 
         const usuarioActualizado = await Usuario.findByIdAndUpdate(uid,campos,{ new : true });
