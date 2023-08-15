a024800aeaa351f4743552a3e07550640045f7c6
crearwebdesign <oscar10034@gmail.com>
2023-08-15 11:09:26 -0400

    MenuDesdeBackend

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,6 +4,7 @@ const bcrypt = require('bcryptjs');
 const Usuario = require('../models/usuario');
 const { generarJWT } = require('../helpers/jwt');
 const { googleVerify } = require('../helpers/google-verify');
+const { getMenuFrontEnd } = require('../helpers/menu-frontend');
 
 
 
@@ -42,7 +43,8 @@ const login = async(req, res = response)=>{
 
         res.status(200).json({
             ok : true,
-            token
+            token,
+            menu : getMenuFrontEnd(usuarioDB.role)
         })
         
     } catch (error) {
@@ -86,7 +88,8 @@ const googleSignIn = async (req, res = response) => {
          res.json({
             ok : true,
             email, name, picture,
-            token
+            token,
+            menu : getMenuFrontEnd(usuario.role)
          })
         
     } catch (error) {
@@ -115,7 +118,8 @@ const renewToken = async (req, res = response) => {
     res.json({
         ok : true,
         token,
-        usuario
+        usuario,
+        menu : getMenuFrontEnd(usuario.role)
     })
 
 };
